4bb0a62553c848329f6b7c5533a3fd5e0801220b
MalkingMan <arrayalkhozini@gmail.com>
2025-11-27 09:57:57 +0700

    Add ContainerApp interface and ContainerDataApp implementation
    Introduce the `ContainerApp` interface to define dependency injection containers and implement `ContainerDataApp` to provide the `RepositoriSiswa` instance using the Room database.

---
+++ app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
@@ -0,0 +1,19 @@
+package com.example.roomdatabase.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.roomdatabase.room.DatabaseSiswa
+
+
+interface ContainerApp {
+    val repositoriSiswa: RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context) :
+    ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
+
