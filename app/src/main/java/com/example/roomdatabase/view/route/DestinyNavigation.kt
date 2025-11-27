ad4b789d435f0bfa86dd043ddb7018d2985fbb3d
MalkingMan <arrayalkhozini@gmail.com>
2025-11-27 12:03:44 +0700

    Enhance DestinyNavigation interface with route and titleRes properties

--- app/src/main/java/com/example/roomdatabase/view/route/DestinyNavigation.kt
+++ app/src/main/java/com/example/roomdatabase/view/route/DestinyNavigation.kt
@@ -1,4 +1,13 @@
 package com.example.roomdatabase.view.route
 
 interface DestinyNavigation {
+    /**
+     * Nama unik yang digunakan sebagai jalur untuk setiap composable.
+     */
+    val route: String
+
+    /**
+     * Resource ID berupa string yang menjadi judul halaman.
+     */
+    val titleRes: Int
 }
